67d5446b48b5a3a77f141b55adc405cf86f0a84f
Richard <Richard>
2020-05-30 12:59:59 -0700

    basic layout

--- src/lib.rs
+++ src/lib.rs
@@ -63,6 +63,10 @@ impl Shoji {
         }))
     }
 
+    pub fn get_node(&mut self,node_index: Index) -> Result<&mut Node,&'static str> {
+        Ok(&mut self.nodes[node_index])
+    }
+
     pub fn layout(&self, i: Index) -> Result<&Layout, &'static str> {
         match &self.nodes[i].layout {
             Some(l) => Ok(l),
@@ -70,8 +74,49 @@ impl Shoji {
         }
     }
 
-    pub fn compute_layout(&mut self, i: Index, s: LayoutSize) -> Result<(), &'static str> {
-        Ok(())
+    pub fn compute_layout(&mut self, node_index: Index, s: LayoutSize) -> Result<(), &'static str> {
+        let node = self.get_node(node_index)?;
+        let children = node.children.clone();
+        match node.style.direction {
+            Direction::LeftRight => {
+                let width = s.width;
+                let height = s.height;
+                match width {
+                    Some(w) => {
+                        let child_width = w/children.len() as f64;
+                        for c in children.iter() {
+                            self.compute_layout(*c, LayoutSize{
+                                width:Some(child_width),
+                                height
+                            })?;
+                        }
+                        Ok(())
+                    },
+                    None => {
+                        Err("cannot compute layout of LeftRight without defined width")
+                    }
+                }
+            },
+            Direction::TopBottom => {
+                let width = s.width;
+                let height = s.height;
+                match height {
+                    Some(h) => {
+                        let child_height = h/children.len() as f64;
+                        for c in children.iter() {
+                            self.compute_layout(*c, LayoutSize{
+                                width,
+                                height:Some(child_height)
+                            })?;
+                        }
+                        Ok(())
+                    },
+                    None => {
+                        Err("cannot compute layout of TopBottom without defined width")
+                    }
+                }
+            }
+        }
     }
 }
 
